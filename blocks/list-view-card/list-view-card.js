e52d95758041bbe084bb902a58d484adf86a6760
Ashwin Jethawa <Ashwin.Jethawa@teknopoint.local>
2025-07-31 18:53:51 +0530

    Added List view

---
+++ blocks/list-view-card/list-view-card.js
@@ -0,0 +1,33 @@
+import {button, div, span, p, img} from "../../scripts/dom-helpers.js"
+export default function decorate(block){
+    console.log(block);
+    let listcontainer = div({class:"list-view-container"},
+        div({class:"list-wrapper"},
+            div({class:"fund-name-wrapper"},
+                div({class:"logo-container"},
+                    img({class: "logoScheme",src: "../../icons/Group.svg",alt: "BrandLogo"})
+                ),
+                div({class:"fund-name-container"},
+                    p("Motilal Oswal"),
+                    label("Nifty 500 Momentum 50 Index Fund")
+                ),
+                img({class: "logoScheme",src: "../../icons/direction-right.svg",alt: "Direction Right"})
+            ),
+            div({class:"cagr-return"},
+                div({class:"cagr-value"},"24.02",span("%")),
+                p("CAGR")
+            ),
+            div({class:"risk-star-icon"},
+                img({class: "riskfactor-icon",src: "../../icons/Risk-o-meter.svg",alt: "risk icon"}),
+                div({class: "star"},
+                    img({class: "star-icon",src: "../../icons/star.svg",alt: "star-icon"}),
+                    img({class: "fillstar-icon",src: "../../icons/star-filled.svg",alt: "fillstar-icon"})
+                )
+            ),
+            div({class:"btn-invest"},
+                button("Invest")
+            )
+        )
+    )
+    return listcontainer
+}
