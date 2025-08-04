1158ac4c8f3f33a9e766e3b16983b924861b76d7
Alok Singh <Alok.Singh@teknopoint.local>
2025-08-04 12:00:59 +0530

    code add watch list comp

--- blocks/watchlist-block/watchlist-block.js
+++ blocks/watchlist-block/watchlist-block.js
@@ -25,51 +25,53 @@ export default function decorate(block) {
     });
   });
   const cardContainer =
-  div({class: 'card-wrapper'},
-    div({class: 'card-left'},
-      div({
+    div({ class: 'card-wrapper' },
+      div({ class: 'card-left' },
+        div({
           class: 'card-left-title'
         },
-        img({
+          img({
             class: "watchlist-logo",
             src: block.querySelector(".watchlist-items1 img").src,
             alt: "BrandLogo"
-        }),
-        div({class:'titlewrapper'},
-            h2({class: 'card-title'}, 
-                block.querySelector(".watchlist-items1 .watchlist-inneritems2").innerText),
-            h2({class: 'card-title-1'},  
-                block.querySelector(".watchlist-items1 .watchlist-inneritems3").innerText),
+          }),
+          div({ class: 'titlewrapper' },
+            h2({ class: 'card-title' },
+              block.querySelector(".watchlist-items1 .watchlist-inneritems2").innerText),
+            h2({ class: 'card-title-1' },
+              block.querySelector(".watchlist-items1 .watchlist-inneritems3").innerText),
+          )
+        ),
+        div({ class: 'button-wrapper' },
+          block.querySelector(".watchlist-items1 .watchlist-inneritems4")
         )
       ),
-      div({class: 'button-wrapper'},
-        block.querySelector(".watchlist-items1 .watchlist-inneritems4")
-      )
-    ),
-    div({class: 'card-right'},
-        div({class: 'upper-right'},
-            div({class:'fund-logo'},
-                img({src: "../../icons/Group.svg", alt: "FundLogo"})
+      div({ class: 'card-coner' },
+        div({ class: 'card-right' },
+          div({ class: 'upper-right' },
+            div({ class: 'fund-logo' },
+              img({ src: "../../icons/Group.svg", alt: "FundLogo" })
             ),
-            div({class:'wishlist-icon'},
-             block.querySelector(".watchlist-items2 .watchlist-inneritems2")   
+            div({ class: 'wishlist-icon' },
+              block.querySelector(".watchlist-items2 .watchlist-inneritems2")
             )
-        ),
-        div({class : 'lower-right'},
-            div({class:'fund-name-card'},
-            p(block.querySelector(".watchlist-items2 .watchlist-inneritems1").innerText),    
-            div({class:'fund-name'},"Asset Allocation Passive Fund Conservative")
+          ),
+          div({ class: 'lower-right' },
+            div({ class: 'fund-name-card' },
+              p(block.querySelector(".watchlist-items2 .watchlist-inneritems1").innerText),
+              div({ class: 'fund-name' }, "Asset Allocation Passive Fund Conservative")
             )
-        ),
-        div({class:'fund-returns'},
+          ),
+          div({ class: 'fund-returns' },
             p("Annualised"),
-            div({class:'returns'},
-                p({class: 'returns-percent'},"24.02"), 
-                span({class:'returns-percentage'}, "%")
+            div({ class: 'returns' },
+              p({ class: 'returns-percent' }, "24.02"),
+              span({ class: 'returns-percentage' }, "%")
             )
-        )
-    )
-  );
-   block.innerHTML = "";
+          ),
+        ),
+      )
+    );
+  block.innerHTML = "";
   block.appendChild(cardContainer);
 }
