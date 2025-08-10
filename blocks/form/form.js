c146b88f17a3a9245dcfff543ba412e28d46c240
Alok Singh <Alok.Singh@teknopoint.local>
2025-08-10 13:45:10 +0530

    growth now json and code change

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -71,7 +71,7 @@ async function handleSubmit(form) {
     }
   } catch (e) {
     //   -next-line no-console
-    console.error(e);
+    // console.error(e);
   } finally {
     form.setAttribute('data-submitting', 'false');
     submit.disabled = false;
@@ -82,7 +82,7 @@ export default async function decorate(block) {
   const links = [...block.querySelectorAll('a')].map((a) => a.href);
   const formLink = links.find((link) => link.startsWith(window.location.origin) && link.endsWith('.json'));
   const submitLink = links.find((link) => link !== formLink);
-  // if (!formLink || !submitLink) return;
+  if (!formLink || !submitLink) return;
 
   const form = await createForm(formLink);// , submitLink);
   block.replaceChildren(form);
--- blocks/growth-now/growth-now.js
+++ blocks/growth-now/growth-now.js
@@ -1,15 +1,14 @@
-/*    */
 import formblock from '../form/form.js';
 
 export default function decorate(block) {
-  console.log(block);
+  // console.log(block);
   formblock(block.querySelector('.button-container'));
-  Array.from(block.children).forEach((row, rowIndex) => {
-    // row.classList.add("fund-us-container");
-    row.classList.add(`fund-sec-${rowIndex + 1}`);
-    // Array.from(row.children).forEach((column, colIndex) => {
-    //     column.classList.add("find-us-container-column");
-    //     column.classList.add(`sebtxt-${colIndex + 1}`);
-    // });
+  Array.from(block.children).forEach((row) => {
+    row.classList.add('fund-us-container');
+    // row.classList.add(`fund-us-container-${rowIndex + 1}`);
+    Array.from(row.children).forEach((column, colIndex) => {
+      // column.classList.add('fund-sec');
+      column.classList.add(`fund-sec-${colIndex + 1}`);
+    });
   });
 }
