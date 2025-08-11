c1e5185c8e032f8cd352b63811f38d287cb0d38f
Yoge93 <yogesh.walwalkar@deptagency.com>
2025-08-11 19:32:19 +0530

    Added growth now js structure

--- blocks/growth-now/growth-now.js
+++ blocks/growth-now/growth-now.js
@@ -1,14 +1,22 @@
-// import formblock from '../form/form.js';
+import formblock from '../form/form.js';
 
 export default function decorate(block) {
-  console.log(block);
-  // Array.from(block.children).forEach((row) => {
-  //   row.classList.add('fund-us-container');
-  //   // row.classList.add(`fund-us-container-${rowIndex + 1}`);
-  //   Array.from(row.children).forEach((column, colIndex) => {
-  //     // column.classList.add('fund-sec');
-  //     column.classList.add(`fund-sec-${colIndex + 1}`);
-  //   });
-  // });
-  // formblock(block.querySelector('.fund-sec-3'));
+  // console.log(block);
+  Array.from(block.children).forEach((row) => {
+    row.classList.add('fund-us-container');
+    // row.classList.add(`fund-us-container-${rowIndex + 1}`);
+    Array.from(row.children).forEach((column, colIndex) => {
+      // column.classList.add('fund-sec');
+      column.classList.add(`fund-sec-${colIndex + 1}`);
+    });
+  });
+  const storeWrapper = document.createElement('div');
+  storeWrapper.classList.add('fund-sec-5');
+  const storeBlock = block.querySelector('.fund-sec-2 ul');
+  storeWrapper.append(storeBlock);
+  block.querySelector('.fund-us-container').appendChild(storeWrapper);
+  // let formwrapper = document.createElement('div');
+  // formwrapper.classList.add('fund-sec-5');
+  const formstore = block.querySelector('.fund-sec-2 .button-container');
+  formblock(formstore);
 }
