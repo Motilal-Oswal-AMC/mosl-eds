d3d55172a5b65171076b94ed9081b0ef82205960
Alok Singh <Alok.Singh@teknopoint.local>
2025-08-11 12:01:40 +0530

    add code calculator & growth now

--- blocks/growth-now/growth-now.js
+++ blocks/growth-now/growth-now.js
@@ -1,8 +1,7 @@
 import formblock from '../form/form.js';
 
 export default function decorate(block) {
-  // console.log(block);
-  formblock(block.querySelector('.button-container'));
+  // console.log(block)
   Array.from(block.children).forEach((row) => {
     row.classList.add('fund-us-container');
     // row.classList.add(`fund-us-container-${rowIndex + 1}`);
@@ -11,4 +10,5 @@ export default function decorate(block) {
       column.classList.add(`fund-sec-${colIndex + 1}`);
     });
   });
+  formblock(block.querySelector('.fund-sec-3'));
 }
