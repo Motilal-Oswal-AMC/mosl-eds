fac468b528db973c62231f57035d261a041772a3
Ashwin Jethawa <Ashwin.Jethawa@teknopoint.local>
2025-08-12 12:39:40 +0530

    Added broden icon and api code

--- blocks/broaden-your-research/broaden-your-research.js
+++ blocks/broaden-your-research/broaden-your-research.js
@@ -7,7 +7,7 @@ import {
 } from '../../scripts/dom-helpers.js';
 import dataCfObj from '../../scripts/dataCfObj.js';
 import Swiper from '../swiper/swiper-bundle.min.js';
-
+import dataMapMoObj from '../../scripts/constant.js'
 // --- CONSTANTS AND STATE ---
 
 /**
@@ -34,13 +34,16 @@ let swiperInstance = null;
  * @returns {HTMLElement} The complete card element.
  */
 function createCardElement(cardData, brandName, iconsTemplate) {
+    let mop = cardData.fundIcon !== undefined ? cardData.fundIcon.split("/").at(-1) : "MO_Midcap_Fund.png";
+    let mopsec = mop.split(".");
+    let mopthree = mopsec[0] + ".svg";
     // 1. Create the card structure WITHOUT the icons.
     const cardElement = div({ class: 'card-wrap' },
         div({ class: 'img-wrapper' },
             div({ class: 'imgelogo' },
                 img({
                     class: 'logoScheme',
-                    src: '../../icons/Group.svg',
+                    src: '../../icons/fundicon'+mopthree,
                     alt: 'BrandLogo',
                     loading: 'lazy',
                 }),
@@ -49,7 +52,7 @@ function createCardElement(cardData, brandName, iconsTemplate) {
         div({ class: 'title-subtitle' },
             p(brandName),
             div({ class: 'title title-logo' },
-                h2('scheme name'), // Or use dynamic data: h2(cardData.name || 'Scheme Name')
+                h2(cardData.schDetail.schemeName), // Or use dynamic data: h2(cardData.name || 'Scheme Name')
             ),
         ),
         // Create an empty container for the icons
@@ -59,6 +62,9 @@ function createCardElement(cardData, brandName, iconsTemplate) {
     // 2. Find the empty container and append the cloned icons. This is the fix.
     const iconsContainer = cardElement.querySelector('.icons-card-wrap');
     if (iconsContainer && iconsTemplate) {
+        let iconsvg = dataMapMoObj["icons-nfo"][cardData.risk.riskType.toLowerCase().replaceAll(" ","-")]+ ".svg"
+        let riskimg = iconsTemplate.querySelector('img')
+        riskimg.src = '../../icons/risk-icon/'+iconsvg;
         iconsContainer.append(iconsTemplate.cloneNode(true));
     }
 
@@ -147,7 +153,6 @@ export default function decorate(block) {
     // 1. Extract templates and data from the initial, author-configured DOM
     const brandName = block.querySelector('p')?.innerText.trim() || 'Motilal Oswal';
     const iconsTemplate = block.querySelector('ul'); // A simpler, more robust selector
-
     if (!iconsTemplate) {
         console.error('Broaden Your Research: Icons template (<ul>) not found in the initial block content.');
         return;
