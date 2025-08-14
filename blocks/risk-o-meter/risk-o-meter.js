2ad08c2f3f7ab2eb14e93f491d418803ade4c505
RupeshDept <rupesh.mahadik@deptagency.com>
2025-08-14 21:25:51 +0530

    added alok code

--- blocks/risk-o-meter/risk-o-meter.js
+++ blocks/risk-o-meter/risk-o-meter.js
@@ -1,3 +1,5 @@
+import dataMapMoObj from '../../scripts/constant.js';
+
 export default function decorate(block) {
   Array.from(block.children).forEach((row, rowIndex) => {
     row.classList.add('risk-meter-cont', `corner-${rowIndex + 1}`);
@@ -10,4 +12,15 @@ export default function decorate(block) {
   Array.from(listBlocks).forEach((ul, index) => {
     ul.classList.add(`risk-meter-corner-cont-ul-${index + 1}`);
   });
+
+  dataMapMoObj.CLASS_PREFIXES = [];
+  dataMapMoObj.CLASS_PREFIXES = ['item'];
+  dataMapMoObj.addIndexed(block.closest('.product-label'));
+
+  const items = block.closest('.product-label');
+  const divwrapper = document.createElement('div');
+  divwrapper.classList.add('risk-wrapper');
+  divwrapper.append(items.querySelector('.item2'));
+  divwrapper.append(items.querySelector('.item3'));
+  items.append(divwrapper);
 }
