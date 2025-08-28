a827b9188b0d754a062c6898000bda56fd917f44
Ashwin Jethawa <Ashwin.Jethawa@teknopoint.local>
2025-08-28 13:23:54 +0530

    si aded

--- blocks/fund-card/fund-card.js
+++ blocks/fund-card/fund-card.js
@@ -32,7 +32,7 @@ function planListEvent(param, block) {
     }
   });
   const [firstReturnYear] = tempReturns;
-  const valRet = dataMapMoObj.selectreturns === '' ? '3 Years' : dataMapMoObj.selectreturns;
+  const valRet = dataMapMoObj.selectreturns === '' ? 'Since Inception' : dataMapMoObj.selectreturns;
   const selectedReturn = valRet; // dataMapMoObj.selectreturns;
   const returnYear = tempReturns.includes(selectedReturn)
     ? selectedReturn
@@ -220,7 +220,7 @@ export default function decorate(block) {
   const classdropdown = DirectPlanlistArr.length !== 0 ? 'flex' : 'none';
   const optionName = DirectPlanlistArr.length !== 0 ? DirectPlanlistArr[0].optionName : '';
   const [fstRetYear] = tempReturns;
-  const valRet = dataMapMoObj.selectreturns === '' ? '3 Years' : dataMapMoObj.selectreturns;
+  const valRet = dataMapMoObj.selectreturns === '' ? 'Since Inception' : dataMapMoObj.selectreturns;
   const returnYear = tempReturns.includes(valRet)
     ? valRet
     : fstRetYear;
