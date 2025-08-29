0d5d110b999e6e6e9b153755aaf75b02f5f6debf
Alok Singh <Alok.Singh@teknopoint.local>
2025-08-29 20:15:28 +0530

    growth now commonet

--- blocks/growth-now/growth-now.js
+++ blocks/growth-now/growth-now.js
@@ -1,14 +1,14 @@
-import formblock from '../form/form.js';
+// import formblock from '../form/form.js';
 
-export default function decorate(block) {
-  // console.log(block)
-  Array.from(block.children).forEach((row, rowIndex) => {
-    row.classList.add('fund-us-container');
-    row.classList.add(`growth-now-row-${rowIndex + 1}`);
-    Array.from(row.children).forEach((column, colIndex) => {
-      // column.classList.add('fund-sec');
-      column.classList.add(`fund-sec-${colIndex + 1}`);
-    });
-  });
-  formblock(block.querySelector('.button-container'));
-}
+// export default function decorate(block) {
+//   // console.log(block)
+//   Array.from(block.children).forEach((row, rowIndex) => {
+//     row.classList.add('fund-us-container');
+//     row.classList.add(`growth-now-row-${rowIndex + 1}`);
+//     Array.from(row.children).forEach((column, colIndex) => {
+//       // column.classList.add('fund-sec');
+//       column.classList.add(`fund-sec-${colIndex + 1}`);
+//     });
+//   });
+//   formblock(block.querySelector('.button-container'));
+// }
