9913e2c35c9d73f1eab346ba5fe4008628edad29
Abhay Singh <Abhay.Singh@teknopoint.local>
2025-09-30 13:10:03 +0530

    djhc

--- blocks/login-page/login-page.js
+++ blocks/login-page/login-page.js
@@ -1,47 +1,47 @@
-import {
-  div,
-  p,
-  input,
-  label,
-  span,
-} from '../../scripts/dom-helpers.js';
-import dataMapMoObj from '../../scripts/constant.js';
+// import {
+//   div,
+//   p,
+//   input,
+//   label,
+//   span,
+// } from '../../scripts/dom-helpers.js';
+// import dataMapMoObj from '../../scripts/constant.js';
 
-export default function decorate(block) {
-  const loginImage = document.querySelector('.login-image-item');
-  dataMapMoObj.CLASS_PREFIXES = ['login-item', 'login-sub-item', 'login-inner-item'];
-  dataMapMoObj.addIndexed(block);
+// export default function decorate(block) {
+//   const loginImage = document.querySelector('.login-image-item');
+//   dataMapMoObj.CLASS_PREFIXES = ['login-item', 'login-sub-item', 'login-inner-item'];
+//   dataMapMoObj.addIndexed(block);
 
-  dataMapMoObj.CLASS_PREFIXES = ['login-image-item', 'login-image-sub-item', 'login-image-inner-item'];
-  dataMapMoObj.addIndexed(loginImage);
+//   dataMapMoObj.CLASS_PREFIXES = ['login-image-item', 'login-image-sub-item', 'login-image-inner-item'];
+//   dataMapMoObj.addIndexed(loginImage);
 
-  const loginPagePan = div(
-    { class: 'main-login-page' },
-    div(
-      { class: 'login-pan-label' },
-      p({ class: 'user-pan-para' }, 'Let’s Get Started'),
-      div(
-        { class: 'user-input-error' },
-        label({ class: 'input-lable' }, 'Enter PAN'),
-        input({ class: 'user-input', placeholder: 'Enter Pan' }),
-        p({ class: 'error-login' }, 'Please enter a valid PAN'),
-      ),
-    ),
+//   const loginPagePan = div(
+//     { class: 'main-login-page' },
+//     div(
+//       { class: 'login-pan-label' },
+//       p({ class: 'user-pan-para' }, 'Let’s Get Started'),
+//       div(
+//         { class: 'user-input-error' },
+//         label({ class: 'input-lable' }, 'Enter PAN'),
+//         input({ class: 'user-input', placeholder: 'Enter Pan' }),
+//         p({ class: 'error-login' }, 'Please enter a valid PAN'),
+//       ),
+//     ),
 
-    div(
-      { class: 'btn-descr' },
-      p({ class: 'auth btn' }, 'Authenticate'),
-      div(
-        { class: 'discr-new-invester' },
-        p({ class: 'discr' }, 'By clicking ‘Continue’, you accept the', span({ class: 'terms-disc' }, 'Terms and conditions'), 'of Motilal Oswal AMC and agree to receive alerts & notifications via Motilal Oswal AMC SMS, Email and Business Whatsapp Account services.'),
-        p({ class: 'new-invest' }, 'Don’t have an account ?', span({ class: 'new-invest-disc' }, 'New Investor')),
-      ),
-    ),
+//     div(
+//       { class: 'btn-descr' },
+//       p({ class: 'auth btn' }, 'Authenticate'),
+//       div(
+//         { class: 'discr-new-invester' },
+//         p({ class: 'discr' }, 'By clicking ‘Continue’, you accept the', span({ class: 'terms-disc' }, 'Terms and conditions'), 'of Motilal Oswal AMC and agree to receive alerts & notifications via Motilal Oswal AMC SMS, Email and Business Whatsapp Account services.'),
+//         p({ class: 'new-invest' }, 'Don’t have an account ?', span({ class: 'new-invest-disc' }, 'New Investor')),
+//       ),
+//     ),
 
-  );
+//   );
 
-  const userLoginPage = document.querySelector('.login-page-wrapper');
+//   const userLoginPage = document.querySelector('.login-page-wrapper');
 
-  userLoginPage.innerHTML = '';
-  userLoginPage.append(loginPagePan);
-}
+//   userLoginPage.innerHTML = '';
+//   userLoginPage.append(loginPagePan);
+// }
