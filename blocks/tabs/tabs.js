8e75c55f7f2857ed779937eb2e0fcc3beb224684
AnuragPandey <anurag.pandey@teknopoint.in>
2025-11-13 17:33:31 +0530

    added

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -2,7 +2,7 @@ import { toClassName } from '../../scripts/aem.js';
 import dataCfObj from '../../scripts/dataCfObj.js';
 import fundCardblock from '../fund-card/fund-card.js';
 import {
-  button, a, div, input, ul, li, img, table, thead, tbody, tr, th, td,
+  button, a, div, input, ul, li, img, table, thead, tbody, tr, th, td, p,
 } from '../../scripts/dom-helpers.js';
 import dataMapMoObj from '../../scripts/constant.js';
 // import moEdge from './popular-acticles.js';
@@ -24,6 +24,10 @@ export default async function decorate(block) {
     tabpanel.setAttribute('aria-hidden', !!i);
     tabpanel.setAttribute('aria-labelledby', `tab-${id}`);
     tabpanel.setAttribute('role', 'tabpanel');
+
+    if (i === 0 && tabpanel.querySelector('.section')) {
+      tabpanel.querySelector('.section').style.display = 'flex';
+    }
 
     // build tab button
     const tabbtn = document.createElement('button');
@@ -754,6 +758,7 @@ export default async function decorate(block) {
     divtab.append(tabList);
     block.prepend(divtab);
   }
+  
   document.addEventListener('click', (event) => {
     document.querySelectorAll('.cagr-container').forEach((el) => {
       if (!el.contains(event.target)) {
@@ -785,6 +790,7 @@ export default async function decorate(block) {
       if (Array.from(elchild.classList).includes('tab-glp-container')) {
         elchild.style.display = 'none';
         elchild.classList.add('hide-section');
+        elchild.remove();
       }
     });
   }
@@ -851,4 +857,70 @@ export default async function decorate(block) {
     });
   }
   // previous studies tab end
+if (block.closest('.previous-studies-tab')) {
+    const mainblk = block.closest('main');
+    const blk = mainblk.querySelectorAll('.prev-studies-wrapper');
+    Array.from(blk).forEach((mainblk) => {
+      // dataMapMoObj.CLASS_PREFIXES = ['prev-studies'];
+      // dataMapMoObj.addIndexed(mainblk);
+      const divwrapper = document.createElement('div');
+      Array.from(mainblk.children).forEach((elblk, index) => {
+        divwrapper.classList.add('prev-main-wrapper');
+        if (index !== 0) {
+          divwrapper.append(elblk);
+        }
+      });
+      mainblk.append(divwrapper);
+    });
+    const panel = block.querySelectorAll('.tabs-panel');
+    Array.from(panel).forEach((inner, index) => {
+      panel.innerHTML = ''
+      inner.append(blk[index]);
+    });
+
+    block.querySelectorAll('.prev-main-wrapper').forEach((eldata) => {
+      const divprev = document.createElement('div');
+      divprev.classList.add('prev-sub');
+      Array.from(eldata.children).forEach((indata, index) => {
+        if (index < (Array.from(eldata.children).length)) {
+          divprev.append(indata);
+        }
+      });
+      eldata.prepend(divprev);
+    });
+
+    // let initSelecTab;
+    const dropdownlist = block.querySelector('.tabs-list');
+    const tabDrodpwon = div(
+      { class: 'tab-dropdown-wrap' },
+      p({ class: 'selected-tab' }),
+      div({ class: 'tab-droplist' }),
+    );
+    tabDrodpwon.querySelector('.tab-droplist').append(dropdownlist);
+    block.prepend(tabDrodpwon);
+
+    const dropdownWrap = mainblk.querySelector('.tab-dropdown-wrap');
+    const dropList = dropdownWrap.querySelector('.tab-droplist');
+    const selectedTab = dropdownWrap.querySelector('.selected-tab');
+    Array.from(dropdownlist.children).forEach((tab) => {
+      if (tab.getAttribute('aria-selected') === 'true') {
+        selectedTab.textContent = tab.textContent;
+      }
+    });
+
+    dropdownWrap.addEventListener('click', (evetn) => {
+      evetn.preventDefault();
+      console.log('droplist : ', dropList.classList);
+      if (!Array.from(dropList.classList).includes('active')) {
+        dropList.classList.add('active');
+      } else {
+        Array.from(dropdownlist.children).forEach((tab) => {
+          if (tab.getAttribute('aria-selected') === 'true') {
+            selectedTab.textContent = tab.textContent;
+          }
+        });
+        dropList.classList.remove('active');
+      }
+    });
+  }
 }
